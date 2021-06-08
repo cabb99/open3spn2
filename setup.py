b165a12e6912b421ade56de920407129b00e7ce0
cabb99 <carlos.bueno@rice.edu>
2021-06-08 00:41:50 -0500

    Adds versioneer to setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,33 @@
-from distutils.core import setup
+"""
+Open3SPN2
+An Implementation of the 3SPN.2 and 3SPN.2C coarse-grained molecular models of DNA in OpenMM.
+"""
+import sys
+from setuptools import setup, find_packages
+import versioneer
+
+print(f"Version {versioneer.get_version()}")
+short_description = __doc__.split("\n")
+
+try:
+    with open("README.md", "r") as handle:
+        long_description = handle.read()
+except:
+    long_description = "\n".join(short_description[2:])
+
 setup(
       name = 'open3SPN2',
       packages = ['open3SPN2'],
-      version = '0.1.3',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
       license='MIT',
-      description = 'Implementation of 3SPN2 and 3SPN2.C forcefields in openmm',
+      description = short_description[0],
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       author = 'Carlos Bueno', 
       author_email = 'carlos.bueno@rice.edu', 
       url = 'https://github.com/cabb99/open3spn2', 
-      download_url = 'https://github.com/cabb99/open3spn2/archive/0.1.3.tar.gz',
+      download_url = 'https://github.com/cabb99/open3spn2/archive/0.2.0.tar.gz',
       keywords = ['dna', 'forcefield', 'openmm'],
       package_data={'open3SPN2': ['3SPN2.conf','3SPN2.xml']},
       include_package_data=True,
