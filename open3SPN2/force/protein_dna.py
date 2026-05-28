7e7b8813d6aa394d0419ff4215b174954e7ca52d
Finley Clark <60708336+ftclark3@users.noreply.github.com>
2026-05-28 14:31:50 -0500

    updated StringProteinDNA with steven's logical changes commented out. this maintains backward compatibility while allowing for future modification

--- open3SPN2/force/protein_dna.py
+++ open3SPN2/force/protein_dna.py
@@ -290,65 +290,48 @@ class AMHgoProteinDNA(ProteinDNAForce):
 #            print(int(CB_protein['index'].values[0]), int(base_DNA['index'].values[0]), [gamma_ij, r_ijN])
 
 class StringProteinDNA(ProteinDNAForce):
-    """ Protein-DNA string potential (Xinyu)"""
-    def __init__(self, dna, protein, r0, chain_protein='A', chain_DNA='B', k_string_PD=10*4.184, protein_seg=False, group=[]):
+    """ Protein-DNA string potential (Xinyu),
+        With Steven's generalizations commented out pending testing."""
+    #def __init__(self, dna, protein, r0, chain_protein='AB', chain_DNA='CD',
+    #             k_string_PD=10*4.184, protein_seg=False, group=[], force_group=19): 
+    def __init__(self, dna, protein, r0, chain_protein='A', chain_DNA='B', 
+                 k_string_PD=10*4.184, protein_seg=False, group=[], force_group=19):
         self.k_string_PD = k_string_PD
         self.chain_protein = chain_protein
         self.chain_DNA = chain_DNA
         self.r0 = r0
         self.protein_seg = protein_seg
         self.group = group
+        self.force_group = force_group
         super().__init__(dna, protein)
 
     def reset(self):
         r0=self.r0
         k_string_PD=self.k_string_PD
         stringForce = openmm.CustomCentroidBondForce(2, f"0.5*{k_string_PD}*(distance(g1,g2)-{r0})^2")
+        stringForce.setForceGroup(self.force_group)
         self.force = stringForce
         print("String_PD bias on: r0, k_string = ", r0, k_string_PD)
 
     def defineInteraction(self):
         atoms = self.dna.atoms.copy()
         atoms['index'] = atoms.index
-        CA_atoms = atoms[(atoms['chainID'] == self.chain_protein) & (atoms['name'] == 'CA') & atoms['resname'].isin(_proteinResidues)].copy()
-        S_atoms = atoms[(atoms['chainID'] == self.chain_DNA) & (atoms['name'] == 'S') & atoms['resname'].isin(_dnaResidues)].copy()
+        CA_atoms = atoms[
+            #atoms['chainID'].isin(list(self.chain_protein)) &
+            (atoms['chainID'] == self.chain_protein) &
+            (atoms['name'] == 'CA') &
+            atoms['resname'].isin(_proteinResidues)
+        ].copy()
+        S_atoms = atoms[
+            #atoms['chainID'].isin(list(self.chain_DNA)) &
+            (atoms['chainID'] == self.chain_DNA) &
+            (atoms['name'] == 'S') &
+            atoms['resname'].isin(_dnaResidues)
+        ].copy()
         CA_index = [int(atom.index) for atom in CA_atoms.itertuples()]
-        if self.protein_seg: self.force.addGroup([CA_index[x] for x in self.group])
-        else:   self.force.addGroup(CA_index)
+        if self.protein_seg:
+            self.force.addGroup([CA_index[x] for x in self.group])
+        else:
+            self.force.addGroup(CA_index)
         self.force.addGroup([int(atom.index) for atom in S_atoms.itertuples()])
-        bondGroups = [0, 1]
-        print(self.force.getGroupParameters(0))
-        print(self.force.getGroupParameters(1))
-
-        self.force.addBond(bondGroups)
-
-
-class String_length_ProteinDNA(ProteinDNAForce):
-    """ Protein-DNA string potential (Xinyu)"""
-    def __init__(self, dna, protein, chain_protein='A', chain_DNA='B', protein_seg=False, group=[], force_group=17):
-        self.force_group = force_group
-        self.chain_protein = chain_protein
-        self.chain_DNA = chain_DNA
-        self.protein_seg = protein_seg
-        self.group = group
-        super().__init__(dna, protein)
-
-    def reset(self):
-        length = openmm.CustomCentroidBondForce(2, "distance(g1,g2)")
-        length.setForceGroup(self.force_group)
-        self.force = length
-
-    def defineInteraction(self):
-        atoms = self.dna.atoms.copy()
-        atoms['index'] = atoms.index
-        CA_atoms = atoms[(atoms['chainID'] == self.chain_protein) & (atoms['name'] == 'CA') & atoms['resname'].isin(_proteinResidues)].copy()
-        S_atoms = atoms[(atoms['chainID'] == self.chain_DNA) & (atoms['name'] == 'S') & atoms['resname'].isin(_dnaResidues)].copy()
-        CA_index = [int(atom.index) for atom in CA_atoms.itertuples()]
-        if self.protein_seg: self.force.addGroup([CA_index[x] for x in self.group])
-        else:   self.force.addGroup(CA_index)
-        self.force.addGroup([int(atom.index) for atom in S_atoms.itertuples()])
-        bondGroups = [0, 1]
-        print(self.force.getGroupParameters(0))
-        print(self.force.getGroupParameters(1))
-
-        self.force.addBond(bondGroups)
+        self.force.addBond([0, 1])
